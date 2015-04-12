22cf7d826935ad621f7c505d6ab52628ab42f62f
niamh <niamh.mccormack6@mail.dcu.ie>
2015-04-12 19:46:20 +0100

    I'm scared of commitment

--- app/src/main/java/niamh/testapp/Create_Event.java
+++ app/src/main/java/niamh/testapp/Create_Event.java
@@ -3,9 +3,11 @@ package niamh.testapp;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,26 +36,64 @@ import android.widget.Toast;
 public class Create_Event extends MainActivity {
 
 
-        private Spinner spinner1, spinner2;
+        private Spinner choose_sport;
         private Button btnSubmit;
+        private TextView createevent;
+        //private Context context;
+        private int byGetOrPost = 0;
+        EditText pace, distance, location, time, date;
+    //flag 0 means get and 1 means post.(By default it is get.)
+    public Create_Event(Spinner choose_sport, Button btnSubmit,
+                          TextView createevent, EditText pace, EditText distance, EditText location, EditText time, EditText date) {
 
-        @Override
+        }
+
+
+       /* @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.create_event);
             addListenerOnButton();
-            addListenerOnSpinnerItemSelection();
-        }
+            addListenerOnSpinnerItemSelection();}
+*/
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_event);
+        createevent = (TextView) findViewById(R.id.createevent);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        location = (EditText)findViewById(R.id.location);
+        time = (EditText)findViewById(R.id.time);
+        distance = (EditText)findViewById(R.id.distance);
+        date = (EditText)findViewById(R.id.date);
+        pace   = (EditText)findViewById(R.id.pace);
+        choose_sport = (Spinner)findViewById(R.id.choose_sport);
+        btnSubmit.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        Log.v("EditText", pace.getText().toString());
+                        Log.v("EditText", location.getText().toString());
+                        Log.v("EditText", distance.getText().toString());
+                        Log.v("EditText", time.getText().toString());
+                        Log.v("EditText", date.getText().toString());
+                        Log.v("Spinner", choose_sport.getSelectedItem().toString());
+                    }
+                });
+    }
 
 
     public void addListenerOnSpinnerItemSelection() {
-        spinner1 = (Spinner) findViewById(R.id.choose_sport);
-        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        choose_sport = (Spinner) findViewById(R.id.choose_sport);
+        choose_sport.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
 
     public void addListenerOnButton() {
 
-        spinner1 = (Spinner) findViewById(R.id.choose_sport);
+        choose_sport = (Spinner) findViewById(R.id.choose_sport);
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -61,15 +101,75 @@ public class Create_Event extends MainActivity {
             //@Override
             public void onClick(View v) {
 
-                Toast.makeText(Create_Event.this,
-                        "OnClickListener : " +
-                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
-                                "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Create_Event.this,
+                                "OnClickListener : " +
+                                        //"\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
+                                        "\nSpinner 2 : "+ String.valueOf(choose_sport.getSelectedItem()),
+                                Toast.LENGTH_SHORT).show();
             }
 
         });
     }
+        //@Override
+        protected String doInBackground(String... arg0) {
+            if(byGetOrPost == 0){ //means by Get Method
+                try{
+                    String sport = String.valueOf(choose_sport.getSelectedItem());
+                    //String time = time;
+                    String link = "http://testtraintogether.site88.net/create.php";
+                    URL url = new URL(link);
+                    HttpClient client = new DefaultHttpClient();
+                    HttpGet request = new HttpGet();
+                    request.setURI(new URI(link));
+                    HttpResponse response = client.execute(request);
+                    BufferedReader in = new BufferedReader
+                            (new InputStreamReader(response.getEntity().getContent()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+                    while ((line = in.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    in.close();
+                    return sb.toString();
+                }catch(Exception e){
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+            else{
+                try{
+                    String username = (String)arg0[0];
+                    String password = (String)arg0[1];
+                    String link="http://testtraintogether.site88.net/create.php";
+                    String data  = URLEncoder.encode("username", "UTF-8")
+                            + "=" + URLEncoder.encode(username, "UTF-8");
+                    data += "&" + URLEncoder.encode("password", "UTF-8")
+                            + "=" + URLEncoder.encode(password, "UTF-8");
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter
+                            (conn.getOutputStream());
+                    wr.write( data );
+                    wr.flush();
+                    BufferedReader reader = new BufferedReader
+                            (new InputStreamReader(conn.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+                    // Read Server Response
+                    while((line = reader.readLine()) != null)
+                    {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+                }catch(Exception e){
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+        }
+
 
 
 
@@ -151,11 +251,7 @@ public class Create_Event extends MainActivity {
             }
         }
     }
-    @Override
-    protected void onPostExecute(String result){
-        this.statusField.setText("Login Successful");
-        this.roleField.setText(result);
-    }*/
+    */
 
 }
 
