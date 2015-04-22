67b190742930bc82fd3a64c99d1fe3e8af86cf8a
niamh <niamh.mccormack6@mail.dcu.ie>
2015-04-22 12:21:53 +0100

    Stupid piece of shit.

--- app/src/main/java/niamh/testapp/CreateEvent.java
+++ app/src/main/java/niamh/testapp/CreateEvent.java
@@ -1,6 +1,7 @@
 package niamh.testapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,13 +14,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
@@ -33,201 +41,137 @@ import android.widget.Toast;
 /**
  * Created by niamh on 4/1/15.
  */
-public class CreateEvent extends MainActivity {
-
-
-    private Spinner choose_sport;
+public class CreateEvent  extends MainActivity {
+    public TextView statusField, resultField;
+    private Spinner sportChoose;
+    private EditText distanceField, paceField, timeField, dateField, locationField;
+    //private EditText username, password;
+    private Context context;
     private Button btnSubmit;
+    private String result;
+    Spinner choose_sport;
+    String sport, distance, pace, date, time, location = null;
     MainActivity main = new MainActivity();
-    //private Context context;
-    private int byGetOrPost = 0;
-    EditText pace, distance, location, time, date;
 
     //flag 0 means get and 1 means post.(By default it is get.)
-    public CreateEvent(Spinner choose_sport, Button btnSubmit,
-                       EditText pace, EditText distance, EditText location, EditText time, EditText date) {
+    /*public CreateEvent(Context context, Button btnSubmit, Spinner sportChoose, EditText distanceField,
+                       EditText paceField, EditText timeField, EditText dateField, EditText locationField,
+            TextView resultField) {
+        // this.context = context;
+        this.context = context;
+        this.btnSubmit = btnSubmit;
+        this.sportChoose = sportChoose;
+        this.distanceField = distanceField;
+        this.paceField = paceField;
+        this.timeField = timeField;
+        this.dateField = dateField;
+        this.locationField = locationField;
+        this.resultField = resultField;
 
     }
-
-
-       /* @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.create_event);
-            addListenerOnButton();
-            addListenerOnSpinnerItemSelection();}
 */
+    public CreateEvent() {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event);
-        btnSubmit = (Button) findViewById(R.id.btnSubmit);
-        location = (EditText) findViewById(R.id.location);
-        time = (EditText) findViewById(R.id.time);
-        distance = (EditText) findViewById(R.id.distance);
-        date = (EditText) findViewById(R.id.date);
-        pace = (EditText) findViewById(R.id.pace);
+
+        context = this;
+        btnSubmit = (Button) findViewById((R.id.btnSubmit));
+        // sportChoose = (Spinner) findViewById(R.id.choose_sport);
+        distanceField = (EditText) findViewById(R.id.distanceField);
+        paceField = (EditText) findViewById(R.id.paceField);
+        timeField = (EditText) findViewById(R.id.timeField);
+        dateField = (EditText) findViewById(R.id.dateField);
+        locationField = (EditText) findViewById(R.id.locationField);
+        resultField = (TextView) findViewById(R.id.resultField);
         choose_sport = (Spinner) findViewById(R.id.choose_sport);
-        btnSubmit.setOnClickListener(
-                new View.OnClickListener() {
-                    public void onClick(View view) {
-                        try {
-                            pace.getText();
-                            location.getText();
-                            distance.getText();
-                            time.getText();
-                            date.getText();
-                            choose_sport.getSelectedItem();
-                        } catch (NumberFormatException ex) {
-                        }
-                    }
-                });
-    }
-
-
-    public void addListenerOnSpinnerItemSelection() {
-        choose_sport = (Spinner) findViewById(R.id.choose_sport);
-        choose_sport.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-    }
-
-    public void addListenerOnButton() {
-
-        choose_sport = (Spinner) findViewById(R.id.choose_sport);
-        btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
 
-            //@Override
-            public void onClick(View v) {
+            // @Override
+            public void onClick(View view) {
+                sport = choose_sport.getSelectedItem().toString();
+                //result = "Creating an event";
+                resultField = (TextView) findViewById(R.id.resultField);
+                distance = distanceField.getText().toString();
+                pace = paceField.getText().toString();
+                time = timeField.getText().toString();
+                date = dateField.getText().toString();
+                location = locationField.getText().toString();
 
-                Toast.makeText(CreateEvent.this,
-                        "OnClickListener : " +
-                                //"\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
-                                "\nSpinner 2 : " + String.valueOf(choose_sport.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
+
+                new InsertData().execute(sport, distance, pace, time, date, location);
+                resultField.setText(result);
+
+
             }
-
         });
     }
-}
-        //@Override
-     /*   protected String doInBackground(String... arg0) {
-                    try{
-                    String link="http://testtraintogether.site88.net/create.php";
-                   // String data  = URLEncoder.encode("location", "UTF-8")
-                 // +          + "=" + URLEncoder.encode(location, "UTF-8");
-                     //   Double data = URLEncoder.encode("pace", "UTF-8")
-                     //           + "=" + URLEncoder.encode(pace, "UTF-8")
-                  //  data += "&" + URLEncoder.encode("pace", "UTF-8")
-                           // + "=" + URLEncoder.encode(pace, "UTF-8");
-                     String data = ;
-                    URL url = new URL(link);
-                    URLConnection conn = url.openConnection();
-                    conn.setDoOutput(true);
-                    OutputStreamWriter wr = new OutputStreamWriter
-                            (conn.getOutputStream());
-                  //  wr.write( data );
-                    wr.flush();
-                    BufferedReader reader = new BufferedReader
-                            (new InputStreamReader(conn.getInputStream()));
-                    StringBuilder sb = new StringBuilder();
-                    String line = null;
-                    // Read Server Response
-                    while((line = reader.readLine()) != null)
-                    {
-                        sb.append(line);
-                        break;
-                    }
-                    return sb.toString();
-                }catch(Exception e){
-                    return new String("Exception: " + e.getMessage());
-                }
-            }
-        }
+    //@Override
 
 
+    class InsertData extends AsyncTask<String, String, String> {
 
 
-/*
+        protected String doInBackground(String... arg0) {
+            try {
 
-        private TextView statusField,roleField;
-    private Context context;
-    private int byGetOrPost = 0;
-    //flag 0 means get and 1 means post.(By default it is get.)
-    public Create_Event(Context context,TextView statusField,
-                          TextView roleField,int flag) {
-        this.context = context;
-        this.statusField = statusField;
-        this.roleField = roleField;
-        byGetOrPost = flag;
-
-    }
-
-
-    protected void onPreExecute(){
-
-    }
-    @Override
-    protected String doInBackground(String... arg0) {
-        if(byGetOrPost == 0){ //means by Get Method
-            try{
-                String username = (String)arg0[0];
-                String password = (String)arg0[1];
-                String link = "http://testtraintogether.site88.net/login.php";
-                URL url = new URL(link);
-                HttpClient client = new DefaultHttpClient();
-                HttpGet request = new HttpGet();
-                request.setURI(new URI(link));
-                HttpResponse response = client.execute(request);
-                BufferedReader in = new BufferedReader
-                        (new InputStreamReader(response.getEntity().getContent()));
-
-                StringBuffer sb = new StringBuffer("");
-                String line="";
-                while ((line = in.readLine()) != null) {
-                    sb.append(line);
-                    break;
-                }
-                in.close();
-                return sb.toString();
-            }catch(Exception e){
-                return new String("Exception: " + e.getMessage());
-            }
-        }
-        else{
-            try{
-                String username = (String)arg0[0];
-                String password = (String)arg0[1];
-                String link="http://testtraintogether.site88.net/login.php";
-                String data  = URLEncoder.encode("username", "UTF-8")
-                        + "=" + URLEncoder.encode(username, "UTF-8");
-                data += "&" + URLEncoder.encode("password", "UTF-8")
-                        + "=" + URLEncoder.encode(password, "UTF-8");
+                String link = "http://testtraintogether.site88.net/create.php";
+                String sport = (String) arg0[0];
+                String distance = (String) arg0[1];
+                String pace = (String) arg0[2];
+                String time = (String) arg0[3];
+                String date = (String) arg0[4];
+                String location = (String) arg0[5];
+                String data = URLEncoder.encode("sport", "UTF-8")
+                        + "=" + URLEncoder.encode(sport, "UTF-8")
+                        + URLEncoder.encode("distance", "UTF-8")
+                        + "=" + URLEncoder.encode(distance, "UTF-8")
+                        + URLEncoder.encode("pace", "UTF-8")
+                        + "=" + URLEncoder.encode(pace, "UTF-8")
+                        + URLEncoder.encode("time", "UTF-8")
+                        + "=" + URLEncoder.encode(time, "UTF-8")
+                        + URLEncoder.encode("date", "UTF-8")
+                        + "=" + URLEncoder.encode(date, "UTF-8")
+                        + URLEncoder.encode("location", "UTF-8")
+                        + "=" + URLEncoder.encode(location, "UTF-8");
+                //  data += "&" + URLEncoder.encode("password", "UTF-8")
+                //        + "=" + URLEncoder.encode(password, "UTF-8");
                 URL url = new URL(link);
                 URLConnection conn = url.openConnection();
                 conn.setDoOutput(true);
                 OutputStreamWriter wr = new OutputStreamWriter
                         (conn.getOutputStream());
-                wr.write( data );
+                wr.write(data);
                 wr.flush();
                 BufferedReader reader = new BufferedReader
                         (new InputStreamReader(conn.getInputStream()));
                 StringBuilder sb = new StringBuilder();
                 String line = null;
                 // Read Server Response
-                while((line = reader.readLine()) != null)
-                {
+                while ((line = reader.readLine()) != null) {
                     sb.append(line);
                     break;
                 }
                 return sb.toString();
-            }catch(Exception e){
+
+            } catch (Exception e) {
                 return new String("Exception: " + e.getMessage());
             }
+
+        }
+
+    protected void onPostExecute(String result) {
+            if (result != null) {
+
+                resultField.setText(result);
+
+            }
+
         }
     }
-    */
 
-
-
+}
--- app/src/main/java/niamh/testapp/SearchEvent.java
+++ app/src/main/java/niamh/testapp/SearchEvent.java
@@ -1,5 +1,3 @@
-package niamh.testapp;
-
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -9,96 +7,126 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 
-/**
- * Created by niamh on 4/14/15.
- */
-public class SearchEvent  extends AsyncTask<String,Void,String> {
+import niamh.testapp.MainActivity;
+import niamh.testapp.R;
 
-    private TextView resultField;
+public class SearchEvent  extends MainActivity {
+    public TextView statusField, resultField;
+    private Spinner sportChoose;
+    private EditText distanceField, paceField, timeField, dateField, locationField;
     //private EditText username, password;
-
-    private int byGetOrPost = 0;
-    //EditText distance;
+    private Context context;
+    private Button btnSubmit;
+    private String result;
+    Spinner choose_sport;
+    String sport, distance, pace, date, time, location = null;
     MainActivity main = new MainActivity();
 
     //flag 0 means get and 1 means post.(By default it is get.)
-    public SearchEvent(
-                          TextView resultField) {
+    /*public CreateEvent(Context context, Button btnSubmit, Spinner sportChoose, EditText distanceField,
+                       EditText paceField, EditText timeField, EditText dateField, EditText locationField,
+            TextView resultField) {
+        // this.context = context;
+        this.context = context;
+        this.btnSubmit = btnSubmit;
+        this.sportChoose = sportChoose;
+        this.distanceField = distanceField;
+        this.paceField = paceField;
+        this.timeField = timeField;
+        this.dateField = dateField;
+        this.locationField = locationField;
+        this.resultField = resultField;
 
-
-        // this.username = username;
-        //this.password = password;
-      //  byGetOrPost = flag;
     }
-
-    protected void onPreExecute() {
-        // username = (EditText)findViewById(R.id.editText1);
+*/
+    public SearchEvent() {
     }
-
-    // @Override
-    protected String doInBackground(String... arg0) {
-                try {
-                    String distance = (String) arg0[0];
-                    // String password = (String) arg0[1];
-                    String link = "http://testtraintogether.site88.net/search.php";
-                    String data = URLEncoder.encode("distance", "UTF-8")
-                            + "=" + URLEncoder.encode(distance, "UTF-8");
-                    //data += "&" + URLEncoder.encode("password", "UTF-8");
-                    //  + "=" + URLEncoder.encode(password, "UTF-8");
-                    URL url = new URL(link);
-                    URLConnection conn = url.openConnection();
-                    conn.setDoOutput(true);
-                    OutputStreamWriter wr = new OutputStreamWriter
-                            (conn.getOutputStream());
-                    wr.write(data);
-                    wr.flush();
-                    BufferedReader reader = new BufferedReader
-                            (new InputStreamReader(conn.getInputStream()));
-                    StringBuilder sb = new StringBuilder();
-                    String line = null;
-                    // Read Server Response
-                    while ((line = reader.readLine()) != null) {
-                        sb.append(line);
-                        break;
-                    }
-                    return sb.toString();
-
-                } catch (Exception e) {
-                    return new String("Exception: " + e.getMessage());
-                }
-            }
-
 
     @Override
-    protected void onPostExecute(String result) {
-        if (result != null) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
 
-            this.resultField.setText(result);
+        context = this;
+        btnSubmit = (Button) findViewById((R.id.btnSubmit));
+        // sportChoose = (Spinner) findViewById(R.id.choose_sport);
+        distanceField = (EditText) findViewById(R.id.distanceField);
+        paceField = (EditText) findViewById(R.id.paceField);
+        timeField = (EditText) findViewById(R.id.timeField);
+        dateField = (EditText) findViewById(R.id.dateField);
+        locationField = (EditText) findViewById(R.id.locationField);
+        resultField = (TextView) findViewById(R.id.resultField);
+        choose_sport = (Spinner) findViewById(R.id.choose_sport);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+            // @Override
+            public void onClick(View view) {
+
+                resultField = (TextView) findViewById(R.id.resultField);
+                distance = distanceField.getText().toString();
+                pace = paceField.getText().toString();
+
+
+                new searchData().execute(distance, pace);
+                resultField.setText(result);
+
+
+            }
+        });
+    }
+    //@Override
+
+
+    class searchData extends AsyncTask<String, String, String> {
+
+
+        protected String doInBackground(String... arg0) {
+            try {
+
+                String link = "http://testtraintogether.site88.net/create.php";
+                String distance = (String) arg0[0];
+                String pace = (String) arg0[1];
+
+                
+                URL url = new URL(link);
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter
+                        (conn.getOutputStream());
+                wr.write(data);
+                wr.flush();
+                BufferedReader reader = new BufferedReader
+                        (new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                // Read Server Response
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                    break;
+                }
+                return sb.toString();
+
+            } catch (Exception e) {
+                return new String("Exception: " + e.getMessage());
+            }
 
         }
 
-    }
-}
+        protected void onPostExecute(String result) {
+            if (result != null) {
 
+                resultField.setText(result);
 
-  /*  @Override
-    protected void onPostExecute(String result){
-        this.statusField.setText("Login Successful");
-        this.roleField.setText(result);
+            }
+
+        }
     }
-}*/
 
